9bd4189c064962fdf3df536f2db1df8fad40a910
Giorgi.Rokhadze <giorgi.rokhadze@twino.eu>
2018-07-12 11:52:23 +0400

    Final commit

--- src/main/resources/WEB-INF/src/app/index.route.js
+++ src/main/resources/WEB-INF/src/app/index.route.js
@@ -11,7 +11,7 @@
     {
         $locationProvider.html5Mode(true);
 
-        $urlRouterProvider.otherwise('/dashboard-project');
+        $urlRouterProvider.otherwise('/contacts');
 
         /**
          * Layout Style Switcher
